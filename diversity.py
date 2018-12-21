ffabf1d25b53d6b8a8bb1a73c626c5ef63fe01bc
Damien <damien.agopian@gmail.com>
2018-12-21 12:03:17 +0900

    Fixed bug for chem diversity calculation

--- diversity.py
+++ diversity.py
@@ -1,26 +1,26 @@
 from rdkit import Chem, DataStructs
 from rdkit.Chem.Fingerprints import FingerprintMols
 import pandas as pd
-
+import argparse
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', type=int, help='Number of islands')
-    parser.add_argument('-a', type=int, default='island',
+    parser.add_argument('-i', type=int, default=0, help='Number of islands')
+    parser.add_argument('-a', type=str, default='island',
                         help='"island" or "chem" : Comput diversity for islands or chemGE')
     args = parser.parse_args()
 
-    N_islands = args.i
+    N_island = args.i
     alg = args.a
     
     ms = []
     if alg == 'island':
-        for i in N_island:
+        for i in range(N_island):
 
             tab = pd.read_csv(str(i)+'_final_pop.csv')
-            ms.append(list(tab.iloc[:,2]))
+            ms += list(tab.iloc[:,2])
 
-        fps = [FingerprintMols.FingerprintMol(x) for x in ms]
+        fps = [FingerprintMols.FingerprintMol(Chem.MolFromSmiles(x)) for x in ms]
 
         t = 0.0
         for x in fps:
@@ -29,10 +29,10 @@ def main():
         print(t/(len(fps)*len(fps)))
     
     if alg == 'chem':
-        tab = pd.read_csv(str(i)+'chemGE.csv')
-            ms.append(list(tab.iloc[:,2]))
+        tab = pd.read_csv('chemGE.csv')
+        ms += list(tab.iloc[:,2])
 
-        fps = [FingerprintMols.FingerprintMol(x) for x in ms]
+        fps = [FingerprintMols.FingerprintMol(Chem.MolFromSmiles(x)) for x in ms]
 
         t = 0.0
         for x in fps:
